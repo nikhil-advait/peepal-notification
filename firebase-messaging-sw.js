1ee00d7723a6eb062e74bb5af23bca6f4b4f3b3a
Nikhil Yeole <nikhil.yeole36@gmail.com>
2022-05-06 14:54:00 +0530

    Clean up and make subscription flow.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,11 +1,3 @@
-/* // Import and configure the Firebase SDK
-// These scripts are made available when the app is served or deployed on Firebase Hosting
-// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
-importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
-
-const messaging = firebase.messaging(); */
 
 /*
  * Here is is the code snippet to initialize Firebase Messaging in the Service
@@ -34,28 +26,50 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 
+
+// Add 'notificationclick' event handler before firebase.messaging() call
+self.addEventListener('notificationclick', function (e) {
+  console.log('SW notification click event', e)
+  console.log('urlToOpen: ', e.notification.data.FCM_MSG.data.urlToOpen)
+  e.notification.close();
+
+  e.waitUntil(clients.openWindow(e.notification.data.FCM_MSG.data.urlToOpen));
+
+  // This is still openeing link in browser browser. 
+  //e.waitUntil(clients.openWindow("intent://8xn9iq3lG_w/#Intent;scheme=vnd.youtube;package=com.google.android.youtube;S.browser_fallback_url=https://www.youtube.com/watch?v=w_f0vwZcRMw;end;"));
+
+  // This works but <a> tag is not possible in service worker file.
+  // <a href="intent://8xn9iq3lG_w/#Intent;scheme=vnd.youtube;package=com.google.android.youtube;S.browser_fallback_url=https://www.youtube.com/watch?v=w_f0vwZcRMw;end;">youtube or market</a>
+
+});
+
+/*
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
  const messaging = firebase.messaging();
  
 
-
 // If you would like to customize notifications that are received in the
 // background (Web app is closed or not in browser focus) then you should
 // implement this optional method.
-// Keep in mind that FCM will still show notification messages automatically 
-// and you should use data messages for custom notifications.
+// Keep in mind that FCM will still show notification messages automatically (i.e. if 'nofification' key is used in payload)
+// and you should use 'data' messages for custom notifications.
 // For more info see: 
 // https://firebase.google.com/docs/cloud-messaging/concept-options
+
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
+  // // Customize notification here
+  // const notificationTitle = 'Background Message Title';
+  // const notificationOptions = {
+  //   body: 'Background Message body.',
+  //   icon: '/firebase-logo.png',
+  //   data: {urlToOpen: payload.data.urlToOpen}
+  // };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  // // Show notification manually
+  // self.registration.showNotification(notificationTitle, notificationOptions);
+
 });
+
+*/
